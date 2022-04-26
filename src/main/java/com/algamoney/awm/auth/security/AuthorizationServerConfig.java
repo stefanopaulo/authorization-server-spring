e2af558b8d560d4b98bc6f74a4654002ec6ec7d0
Stefano Paulo <stefanopaulo47@gmail.com>
2022-04-25 21:07:21 -0300

    Spring Security project with OAuth2: implementing Password Flow, Client Credentials, Implicit Grant and Authorization Code with PKCE. No JWT.

---
+++ src/main/java/com/algamoney/awm/auth/security/AuthorizationServerConfig.java
@@ -0,0 +1,93 @@
+package com.algamoney.awm.auth.security;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.CompositeTokenGranter;
+import org.springframework.security.oauth2.provider.TokenGranter;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+	
+	private final PasswordEncoder passwordEncoder;
+	private final AuthenticationManager authenticationManager;
+	private final UserDetailsService userDetailsService;
+
+	public AuthorizationServerConfig(PasswordEncoder passwordEncoder, AuthenticationManager authenticationManager, UserDetailsService userDetailsService) {
+		this.passwordEncoder = passwordEncoder;
+		this.authenticationManager = authenticationManager;
+		this.userDetailsService = userDetailsService;
+	}
+	
+	@Override
+	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+		clients
+			.inMemory()
+				.withClient("angular")
+					.secret(passwordEncoder.encode("@ngul@r0"))
+					.authorizedGrantTypes("password", "refresh_token")
+					.scopes("write", "read")
+					.accessTokenValiditySeconds(6 * 60 * 60)
+					.refreshTokenValiditySeconds(3600 * 24)
+			.and()
+				.withClient("mobile")
+					.secret("m0b1l3#")
+					.authorizedGrantTypes("password", "refresh_token")
+					.scopes("write", "read")
+					.accessTokenValiditySeconds(6 * 60 * 60)
+					.refreshTokenValiditySeconds(3600 * 24)
+			.and()
+				.withClient("moneyanalitics")
+					.secret(passwordEncoder.encode(""))
+					.authorizedGrantTypes("authorization_code")
+					.scopes("write", "read")
+					.redirectUris("http://client-application")
+					.accessTokenValiditySeconds(6 * 60 * 60)
+					.refreshTokenValiditySeconds(3600 * 24)
+			.and()
+				.withClient("faturamento")
+					.secret(passwordEncoder.encode("faturamento123"))
+					.authorizedGrantTypes("client_credentials")
+					.scopes("read")
+			.and()
+				.withClient("checktoken")
+					.secret(passwordEncoder.encode("check123"));
+	}
+	
+	@Override
+	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+		endpoints
+			.authenticationManager(authenticationManager)
+			.userDetailsService(userDetailsService)
+			.reuseRefreshTokens(false)
+			.tokenGranter(tokenGranter(endpoints));
+	}
+	
+	@Override
+	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+		security.checkTokenAccess("permitAll()")
+				.allowFormAuthenticationForClients();
+	}
+	
+	private TokenGranter tokenGranter(AuthorizationServerEndpointsConfigurer endpoints) {
+		PkceAuthorizationCodeTokenGranter pkceAuthorizationCodeTokenGranter = new PkceAuthorizationCodeTokenGranter(endpoints.getTokenServices(),
+				endpoints.getAuthorizationCodeServices(), endpoints.getClientDetailsService(),
+				endpoints.getOAuth2RequestFactory());
+		
+		List<TokenGranter> granters = Arrays.asList(
+				pkceAuthorizationCodeTokenGranter, endpoints.getTokenGranter());
+		
+		return new CompositeTokenGranter(granters);
+	}
+
+}
---
+++ src/main/java/com/algamoney/awm/auth/security/WebSecurityConfig.java
@@ -0,0 +1,42 @@
+package com.algamoney.awm.auth.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+			.withUser("admin")
+				.password(passwordEncoder().encode("admin"))
+				.roles("ADMIN");
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	@Override
+	protected AuthenticationManager authenticationManager() throws Exception {
+		return super.authenticationManager();
+	}
+
+	@Bean
+	@Override
+	protected UserDetailsService userDetailsService() {
+		return super.userDetailsService();
+	}
+
+}
